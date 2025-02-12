996d4942b099b2cabf0b0abebad0082c36b3c892
Athos <athosbransk@gmail.com>
2025-02-11 23:09:26 -0300

    Starting fastq file metadata creation

--- 5-differential_expression_analysis.R
+++ 5-differential_expression_analysis.R
@@ -1,35 +1,43 @@
 library("DESeq2")
 library("tidyverse")
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("DESeq2")
+
 data <- read.csv("raw_counts.csv", header = T, row.names = "ensembl_id")
 data <- data[,sort(colnames(data))]
 head(data)
-colSums(data)
 
-# identify biological replicates
-condition <- c(rep("LNCAP_Hypoxia", 2), 
-               rep("LNCAP_Normoxia", 2), 
-               rep("PC3_Hypoxia", 2), 
-               rep("PC3_Normoxia", 2))
+# TUTORIAL - VIDEO 
 
-my_colData <- as.data.frame(condition)
-rownames(my_colData) <- colnames(data)
-my_colData
+count_data <- read.csv("bulk_data/auto_anti_pdl1/58_ADJ.tabular", sep = "\t")
+colnames(count_data)[2] <- "counts"
+count_data$condition <- "Anti-PDL1"
+count_data$tissue <- "Adjacent"
+head(count_data)
 
-dds <- DESeqDataSetFromMatrix(countData = data,
-                              colData = my_colData,
-                              design = ~condition)
+count_data2 <- read.csv("bulk_data/auto_anti_pdl1/58_TUM.tabular", sep = "\t")
+colnames(count_data2)[2] <- "counts"
+count_data2$condition <- "Anti-PDL1"
+count_data2$tissue <- "Tumor"
+head(count_data2)
 
-dds <- DESeq(dds)
-dds
+library(EnsDb.Hsapiens.v79)
 
-head(dds@assays@data$counts)
+# 1. Convert from ensembl.gene to gene.symbol
+ensembl.genes <- c("ENSG00000150676", "ENSG00000099308", "ENSG00000142676", "ENSG00000180776", "ENSG00000108848", "ENSG00000277370", "ENSG00000103811", "ENSG00000101473")
 
-normalized_counts <- counts(dds, normalized = T)
+geneIDs1 <- ensembldb::select(EnsDb.Hsapiens.v79, keys= ensembl.genes, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
 
-head(normalized_counts)
-annotation <- read.csv("GRCh38.p13_annotation.csv", header = T, stringsAsFactors = F)
-head(annotation)
+result_table
+
+# Create dataframe
+df <- data.frame(id=c(1,2,3,NA),
+                 address=c('Orange St','Anton Blvd','Jefferson Pkwy',''),
+                 work_address=c('Main St',NA,'Apple Blvd','Portola Pkwy'))
+df$address[df$address == 'Orange St'] <- 'Portola Pkwy'
 
 
 
