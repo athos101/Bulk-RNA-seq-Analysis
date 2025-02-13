8cba1dd9374282f7a9dbbb4db95e201bbd994efb
Athos <athosbransk@gmail.com>
2025-02-12 22:23:52 -0300

    implementing gene ensambl to symbol

--- 5-differential_expression_analysis.R
+++ 5-differential_expression_analysis.R
@@ -1,43 +1,51 @@
 library("DESeq2")
 library("tidyverse")
+library("org.Hs.eg.db")
+library("dplyr")
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("DESeq2")
 
-data <- read.csv("raw_counts.csv", header = T, row.names = "ensembl_id")
-data <- data[,sort(colnames(data))]
-head(data)
-
-# TUTORIAL - VIDEO 
-
-count_data <- read.csv("bulk_data/auto_anti_pdl1/58_ADJ.tabular", sep = "\t")
-colnames(count_data)[2] <- "counts"
+# READING THE CSV FILES
+count_data <- read.csv("bulk_data/auto_anti_pdl1/58_ADJ.tabular", sep = "\t", row.names="Geneid")
+# RENAMING COUNT COLUMN TO COUNTS
+colnames(count_data)[1] <- "counts"
+# ADDING CONDITION AND TISSUE INFO
 count_data$condition <- "Anti-PDL1"
 count_data$tissue <- "Adjacent"
+# SORTING COLUMNS SO THAT ALL CSV COLUMNS READ WILL BE IN THE SAME ORDER
+count_data <- count_data[,sort(colnames(count_data))]
 head(count_data)
 
-count_data2 <- read.csv("bulk_data/auto_anti_pdl1/58_TUM.tabular", sep = "\t")
-colnames(count_data2)[2] <- "counts"
+count_data2 <- read.csv("bulk_data/auto_anti_pdl1/58_TUM.tabular", sep = "\t", row.names="Geneid")
+# RENAMING COUNT COLUMN TO COUNTS
+colnames(count_data2)[1] <- "counts"
+# ADDING CONDITION AND TISSUE INFO
 count_data2$condition <- "Anti-PDL1"
-count_data2$tissue <- "Tumor"
+count_data2$tissue <- "Adjacent"
+# SORTING COLUMNS SO THAT ALL CSV COLUMNS READ WILL BE IN THE SAME ORDER
+count_data2 <- count_data2[,sort(colnames(count_data2))]
 head(count_data2)
-
-library(EnsDb.Hsapiens.v79)
+# CONVERTING ENSAMBL TO GENE SYMBOL
+if(substr(rownames(count_data2)[1],1,4)=="ENSG"){
+  # CLEAN SUFFIX
+  rownames(count_data2) <- gsub("\\..*", "", rownames(count_data2))
+  head(count_data2)
+  # CONVERT ENSEMBL TO SYMBOL
+  count_data2 <- mapIds(org.Hs.eg.db,keys=count_data2$Ensembl,keytsub(ype = "ENSEMBL",column="SYMBOL"))
+}
 
 # 1. Convert from ensembl.gene to gene.symbol
-ensembl.genes <- c("ENSG00000150676", "ENSG00000099308", "ENSG00000142676", "ENSG00000180776", "ENSG00000108848", "ENSG00000277370", "ENSG00000103811", "ENSG00000101473")
+colnames(count_data2)[1] <- "ensembl"
+count_data2 <- mapIds(org.Hs.eg.db,keys=count_data2$Ensembl,keytsub(ype = "ENSEMBL",column="SYMBOL"))
+?mapIds
 
-geneIDs1 <- ensembldb::select(EnsDb.Hsapiens.v79, keys= ensembl.genes, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
 
-result_table
 
-# Create dataframe
-df <- data.frame(id=c(1,2,3,NA),
-                 address=c('Orange St','Anton Blvd','Jefferson Pkwy',''),
-                 work_address=c('Main St',NA,'Apple Blvd','Portola Pkwy'))
-df$address[df$address == 'Orange St'] <- 'Portola Pkwy'
+
+
 
 
 
