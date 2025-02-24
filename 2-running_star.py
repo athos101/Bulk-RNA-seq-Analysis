1fb0ff86190243defa3e712d87d653f6bd5fab2e
athos101 <athosbransk@gmail.com>
2025-02-24 15:14:59 -0300

    fixing star command

--- 2-running_star.py
+++ 2-running_star.py
@@ -6,12 +6,13 @@ output_directory = sys.argv[3]
 genome_directory = sys.argv[1]
 fastq_directory = sys.argv[2]
 
-subprocess.call(f"STAR --runThreadN 64 --runMode genomeGenerate --genomeDir {genome_directory} --genomeFastaFiles {genome_directory}/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa --sjdbGTFfile {genome_directory}/Homo_sapiens.GRCh38.113.gtf", shell=True)
-
-os.mkdir(output_directory)
+#subprocess.call(f"STAR --runThreadN 16 --runMode genomeGenerate --genomeDir {genome_directory} --genomeFastaFiles {genome_directory}/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa --sjdbGTFfile {genome_directory}/Homo_sapiens.GRCh38.113.gtf", shell=True)
+if (not os.path.isdir(output_directory)):
+    os.mkdir(output_directory)
 for fastq in os.listdir(fastq_directory):
-    if fastq.endswith('.fastq.gz'):
-        prefix=fastq.strip(".fastq.gz") + "_output"
+    if fastq.endswith('.fastq'):
+        prefix=fastq.strip(".fastq") + "_output"
+        print("THE PREFIX IS:"+prefix)
         os.mkdir(output_directory + prefix)
         print ("Currently mapping: " + fastq)
-        subprocess.call("STAR --runThreadN 64 --genomeDir /" + genome_directory + " --readFilesCommand zcat --outFilterType BySJout --outFilterMismatchNoverLmax 0.04 --outFilterMismatchNmax 999 --alignSJDBoverhangMin 1 --alignSJoverhangMin 8 --outFilterMultimapNmax 20 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --readFilesIn /"+ fastq_directory + fastq + " --clip3pAdapterSeq GATCGGAAGAGCACACGTCTGAACTCCAGTCAC --outSAMtype BAM SortedByCoordinate --quantMode GeneCounts --outFileNamePrefix " + output_directory + prefix + "/", shell=True)
+        subprocess.call("STAR --runThreadN 16 --genomeDir /" + genome_directory + " --readFilesCommand zcat --outFilterType BySJout --outFilterMismatchNoverLmax 0.04 --outFilterMismatchNmax 999 --alignSJDBoverhangMin 1 --alignSJoverhangMin 8 --outFilterMultimapNmax 20 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --readFilesIn /"+ fastq_directory + fastq + " --clip3pAdapterSeq GATCGGAAGAGCACACGTCTGAACTCCAGTCAC --outSAMtype BAM SortedByCoordinate --quantMode GeneCounts --outFileNamePrefix " + output_directory +"/"+ prefix + "/", shell=True)
