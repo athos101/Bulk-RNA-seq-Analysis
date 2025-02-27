769617c9f5a87e4f64d5ae15b25eb4f6f13074c5
Athos <athosbransk@gmail.com>
2025-02-27 15:36:29 -0300

    Fixing gene symbols duplicates

--- 4-differential_expression_analysis.R
+++ 4-differential_expression_analysis.R
@@ -2,34 +2,56 @@ library("tidyverse")
 library("dplyr")
 library("purrr")
 library("biomaRt")
+library("org.Hs.eg.db")
+library("edgeR")
 
-# Lista de valores a serem buscados
-valores <- duplicated_symbols
-
-# Itera sobre cada valor e imprime os índices onde X tem esse valor
-for (valor in valores) {
-  indices <- rownames(count_data)[count_data$Geneid == valor]  # Obtém os índices
-  print(paste0(valor, ": [", paste(indices, collapse = ", "), "]"))
-}
-
-ensambl_to_gene <- function(count_data, mart){
+remove_ensambl_suffix <- function(count_data){
   count_data$Geneid <- gsub("\\..*", "", rownames(count_data))
   duplicated_ids <- any(duplicated(count_data$Geneid))
   
   if(duplicated_ids){
-    cat("There are duplicated gene symbols in the data.\n")
-    # Show the duplicated gene symbols
-    duplicated_symbols <- count_data$Geneid[duplicated(count_data$Geneid)]
-    print(duplicated_symbols)
     count_data <- count_data %>%
       group_by(Geneid) %>% 
       summarise(across(everything(), sum))
-  } else {
-    cat("There are no duplicated gene symbols in the data.\n")
+      remove(duplicated_ids)
   }
   
   count_data <- as.data.frame(count_data)
   rownames(count_data) <- count_data$Geneid
+  count_data <- subset(count_data, select=-c(Geneid))
+  return(count_data)
+}
+
+ensambl_to_gene <- function(count_data){
+  count_data <- remove_ensambl_suffix(count_data)
+  count_data$symbol <- mapIds(org.Hs.eg.db, keys = rownames(count_data), keytype = "ENSEMBL", column = "SYMBOL")
+  filtered_counts <- count_data
+  na_count <- sum(is.na(filtered_counts))
+  cat("Number of NAs in the gene symbols column:", na_count, "\n")
+  filtered_counts <- filtered_counts[!is.na(filtered_counts$symbol),]
+  dim(filtered_counts)
+  
+  duplicated_genes <- any(duplicated(filtered_counts$symbol))
+  if(duplicated_genes){
+    cat("There are duplicated gene symbols in the data. \n")
+    duplicated_symbols <- filtered_counts$symbol[duplicated(filtered_counts$symbol)]
+    print(duplicated_symbols)
+  } else {
+    cat("There are no duplicated gene symbols in the data.\n")
+  }
+  
+  duplicates <- duplicated(filtered_counts$symbol) | duplicated(filtered_counts$symbol, fromLast = TRUE)
+  
+  filtered_counts <- filtered_counts %>%
+    group_by(symbol) %>%
+    summarise(across(everything(), sum))
+  
+  count_data <- as.data.frame(filtered_counts)
+  return(count_data)
+}
+
+ensambl_to_gene_alt <- function(count_data, mart){
+  count_data<-remove_ensambl_duplicates(count_data)
   
   # RETRIEVE BIOMART DATABASE INFORMATION
   gene_annotations <- getBM(
@@ -51,44 +73,61 @@ ensambl_to_gene <- function(count_data, mart){
 ## ACTUAL LOOP HERE
 
 csv_data_folder <- "bulk_data"
-target_path <- file.path(getwd(), csv_data_folder)
+csv_data_folder <- file.path(getwd(), csv_data_folder)
 
-if(!dir.exists(target_path)){
-  stop(paste("Folder not found: ", target_path))
+if(!dir.exists(csv_data_folder)){
+  stop(paste("Folder not found: ", csv_data_folder))
 }
 
-condition_folders<-list.dirs(target_path, recursive=FALSE)
+condition_folders <- list.dirs(csv_data_folder, recursive=FALSE)
 
-# MART - HUMAN GENES FOR ANNOTATION
-mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl", host = "https://useast.ensembl.org") 
-
-merged_counts <- data.frame()
+bulk_list <- list()
 
 for(subfolder in condition_folders){
   condition <- basename(subfolder)
-  files_in_subfolder <- lapply(subfolder, list.files, full.names=TRUE)
-  for(file in files_in_subfolder[[1]]){
+  csv_files <- list.files(path = subfolder, pattern = "\\.tabular$", full.names = TRUE)
+  
+  for(file in csv_files){
     # READS THE FILE
     print(paste("Reading file: ",basename(file)))
     count_data <- read.csv(file, sep = "\t")
     # RENAME THE COUNTS COLUM TO THE NAME OF THE SAMPLE
     file_name_info <- strsplit(basename(file),"\\.")
-    file_name_info <- paste("Pul_",file_name_info[[1]][1],"_",condition)
+    file_name_info <- paste("Pul",file_name_info[[1]][1],"_",condition, sep = "")
     colnames(count_data)[2] <- file_name_info
     rownames(count_data)<-count_data$Geneid
     count_data<-subset(count_data, select = -c(Geneid))
-    if(dim(merged_counts)[1]==0){
-      merged_counts<-count_data
-    }else{
-      merged_counts<-cbind(merged_counts, count_data)
+    
+    if(length(grep("ENSG", rownames(count_data)[1],fixed = TRUE))){
+      # MART - HUMAN GENES FOR ANNOTATION - only for the ensambl_to_gene_alt
+      #mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl", host = "https://useast.ensembl.org")
+      count_data <- ensambl_to_gene(count_data = count_data)
+    } else {
+      count_data$symbol <- rownames(count_data)
+      count_data <- as.data.frame(count_data)
     }
+    
+    bulk_list[[file_name_info]]<-count_data
   }
 }
 
-merged_counts <- ensambl_to_gene(merged_counts, mart)
+merged_bulks <- Reduce(function(x,y) merge(x,y,by="symbol", all=TRUE), bulk_list)
+rownames(merged_bulks) <- merged_bulks$symbol
+merged_bulks <- subset(merged_bulks, select=-c(symbol))
+
+# Some ensambl_ids have duplicated values after removing the suffix.
+# It was found that all these suffixes were PARY genes, homologous genes
+# of chromossomes X and Y. We just sum them. You don't need to execute this
+# on the pipeline (:
+valores <- duplicated_symbols
+for (valor in valores) {
+  indices <- rownames(count_data)[count_data$Geneid == valor]  # Obtém os índices
+  print(paste0(valor, ": [", paste(indices, collapse = ", "), "]"))
+}
+
+saveRDS(merged_bulks, file = 'bulk_data/merged_bulks_clean.rds')
 
 ## CODE OF WILCOXON + EDGER TMM
-library(edgeR)
 
 # read data
 readCount <- read.table(file = "examples/examples.countMatrix.tsv", header = T, row.names = 1, stringsAsFactors = F, check.names = F)
