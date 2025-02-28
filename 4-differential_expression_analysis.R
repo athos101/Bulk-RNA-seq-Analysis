4046816e5d299156572e18c002958f9e5a40fa6f
athos101 <athosbransk@gmail.com>
2025-02-28 17:09:33 -0300

    Finished Wilcoxon runk sum tutorial

--- 4-differential_expression_analysis.R
+++ 4-differential_expression_analysis.R
@@ -125,40 +125,68 @@ for (valor in valores) {
   print(paste0(valor, ": [", paste(indices, collapse = ", "), "]"))
 }
 
+merged_bulks[is.na(merged_bulks)] <- 0
+
 saveRDS(merged_bulks, file = 'bulk_data/merged_bulks_clean.rds')
+readCount <- readRDS(file = "merged_bulks_clean.rds")
+
+## REPRODUCING THE WILCOXON + EDGER TMM
+
+get_condition <- function(string){
+  if(grepl("ADJ",string, fixed = TRUE))
+    return("Adjacent")
+  if(grepl("TUM",string, fixed = TRUE))
+    return("Tumor")
+  if(grepl("LINFO",string, fixed = TRUE))
+    return("LymphNode")
+}
+
+conditions <- data.frame(1)
+for(col in colnames(readCount)){
+  #conditions[col] <- get_condition(col)
+  conditions[col] <- str_split(col, "_")[[1]][3]
+}
+conditions <- conditions[2:(length(colnames(readCount))+1)]
+conditions <- factor(t(conditions))
 
 ## CODE OF WILCOXON + EDGER TMM
 
-# read data
+# Read count matrix: genes for rows and samples for clumns.
 readCount <- read.table(file = "examples/examples.countMatrix.tsv", header = T, row.names = 1, stringsAsFactors = F, check.names = F)
+# Conditions is a single row with the condition labels for each sample present in the read count matrix.
 conditions <- read.table(file = "examples/examples.conditions.tsv", header = F)
 conditions <- factor(t(conditions))
-# edgeR TMM normalize
-y <- DGEList(counts = readCount, group = conditions)
-## Remove rows conssitently have zero or very low counts
-keep <- filterByExpr(y)
-y <- y[keep, keep.lib.sizes = FALSE]
-## Perform TMM normalization and convert to CPM (Counts Per Million)
-y <- calcNormFactors(y, method = "TMM")
-count_norm <- cpm(y)
-count_norm <- as.data.frame(count_norm)
-# Run the Wilcoxon rank-sum test for each gene
-pvalues <- sapply(1:nrow(count_norm), function(i){
-  data <- cbind.data.frame(gene = as.numeric(t(count_norm[i,])), conditions)
-  p <- wilcox.test(gene~conditions, data)$p.value
-  return(p)
-})
-fdr <- p.adjust(pvalues, method = "fdr")
-# Calculate the fold-change for each gene
-conditionsLevel <- levels(conditions)
-dataCon1 <- count_norm[,c(which(conditions==conditionsLevel[1]))]
-dataCon2 <- count_norm[,c(which(conditions==conditionsLevel[2]))]
-foldChanges <- log2(rowMeans(dataCon2)/rowMeans(dataCon1))
-# Output results based on the FDR threshold 0.05
-outRst <- data.frame(log2foldChange = foldChanges, pValues = pvalues, FDR = fdr)
-rownames(outRst) <- rownames(count_norm)
-outRst <- na.omit(outRst)
-fdrThres <- 0.05
-write.table(outRst[outRst$FDR<fdrThres,], file = "examples/examples.WilcoxonTest.rst.tsv", sep="\t", quote = F, row.names = T, col.names = T)
+
+generate_wilcoxon_table <- function(readCount, conditions){
+  # edgeR TMM normalize
+  y <- DGEList(counts = readCount, group = conditions)
+  ## Remove rows conssitently have zero or very low counts
+  keep <- filterByExpr(y)
+  y <- y[keep, keep.lib.sizes = FALSE]
+  ## Perform TMM normalization and convert to CPM (Counts Per Million)
+  y <- calcNormFactors(y, method = "TMM")
+  count_norm <- cpm(y)
+  count_norm <- as.data.frame(count_norm)
+  # Run the Wilcoxon rank-sum test for each gene
+  pvalues <- sapply(1:nrow(count_norm), function(i){
+    data <- cbind.data.frame(gene = as.numeric(t(count_norm[i,])), conditions)
+    p <- wilcox.test(gene~conditions, data)$p.value
+    return(p)
+  })
+  fdr <- p.adjust(pvalues, method = "fdr")
+  # Calculate the fold-change for each gene
+  conditionsLevel <- levels(conditions)
+  dataCon1 <- count_norm[,c(which(conditions==conditionsLevel[1]))]
+  dataCon2 <- count_norm[,c(which(conditions==conditionsLevel[2]))]
+  
+  # IF >0, IT IS MORE EXPRESSED IN DATACON2. ELSE, IT IS MORE EXPRESSED IN DATACON1. 
+  foldChanges <- log2(rowMeans(dataCon1)/rowMeans(dataCon2))
+  # Output results based on the FDR threshold 0.05
+  outRst <- data.frame(log2foldChange = foldChanges, pValues = pvalues, FDR = fdr)
+  rownames(outRst) <- rownames(count_norm)
+  outRst <- na.omit(outRst)
+  fdrThres <- 0.05
+  write.table(outRst[outRst$FDR<fdrThres,], file = "WilcoxonTest_Clinical.rst.tsv", sep="\t", quote = F, row.names = T, col.names = T)
+}
 
 
