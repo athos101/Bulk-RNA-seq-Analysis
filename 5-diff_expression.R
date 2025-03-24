d61d914df694592b87ab3dc2477cf94d80b20ca9
athos101 <athosbransk@gmail.com>
2025-03-24 17:55:21 -0300

    complete deseq2 implementation

--- 5-diff_expression.R
+++ 5-diff_expression.R
@@ -92,7 +92,7 @@ wilcox_tum <- generate_wilcoxon_table(bulk_tums, conditions_tum)
 write.table(wilcox_tum, file = "WCX_tum_rst.tsv", sep="\t", quote = F, row.names = T, col.names = T)
 
 
-# //-=/-=/-=/-=/-=/-=/ DESEQ2 \=-\=-\=-\=-\=-\=-\=-\=-||
+# //-=/-=/-=/-=/-=/-=/-=/ DESEQ2 \=-\=-\=-\=-\=-\=-\=-\=-\=-\=-||
 library("DESeq2")
 # identify biological replicates
 deseq_conditions <- lapply(colnames(readCount), function(name){
@@ -105,13 +105,113 @@ deseq_conditions <- lapply(colnames(readCount), function(name){
 my_colData <- as.data.frame(deseq_conditions)
 my_colData <- t(my_colData)
 rownames(my_colData) <- colnames(readCount)
-colnames(my_colData)[1] <- "condition"
+colnames(my_colData)[1] <- "deseq_conditions"
+deseq_conditions <- as.factors(deseq_conditions)
 
 dds <- DESeqDataSetFromMatrix(countData = readCount,
                               colData = my_colData,
                               design = ~deseq_conditions)
 
 dds <- DESeq(dds)
+normalized_counts <- counts(dds, normalized = T)
+vsd <- vst(dds, blind = TRUE)
+plotDists = function (vsd.obj) {
+  sampleDists <- dist(t(assay(vsd.obj)))
+  sampleDistMatrix <- as.matrix( sampleDists )
+  rownames(sampleDistMatrix) <- paste( vsd.obj$condition )
+  colors <- colorRampPalette( rev(RColorBrewer::brewer.pal(9, "Blues")) )(255)
+  pheatmap::pheatmap(sampleDistMatrix,
+                     clustering_distance_rows = sampleDists,
+                     clustering_distance_cols = sampleDists,
+                     col = colors)
+}
+plotDists(vsd)
+
+variable_gene_heatmap <- function (vsd.obj, num_genes = 500, title = "") {
+  brewer_palette <- "RdBu"
+  # Ramp the color in order to get the scale.
+  ramp <- colorRampPalette( RColorBrewer::brewer.pal(11, brewer_palette))
+  mr <- ramp(256)[256:1]
+  # get the stabilized counts from the vsd object
+  stabilized_counts <- assay(vsd.obj)
+  # calculate the variances by row(gene) to find out which genes are the most variable across the samples.
+  row_variances <- rowVars(stabilized_counts)
+  # get the top most variable genes
+  top_variable_genes <- stabilized_counts[order(row_variances, decreasing=T)[1:num_genes],]
+  # subtract out the means from each row, leaving the variances for each gene
+  top_variable_genes <- top_variable_genes - rowMeans(top_variable_genes, na.rm=T)
+  # reconstruct colData without sizeFactors for heatmap labeling
+  coldata <- as.data.frame(vsd.obj@colData)
+  coldata$sizeFactor <- NULL
+  # draw heatmap using pheatmap
+  pheatmap::pheatmap(top_variable_genes, color = mr, annotation_col = coldata, fontsize_col = 8, fontsize_row = 300/num_genes, border_color = NA, main = title)
+}
+
+variable_gene_heatmap(vsd, num_genes = 40)
+
+# //-=/-=/-=/-=/-=/-=/-=/ PLOTTING PCA \=-\=-\=-\=-\=-\=-\=-\=-\=-\=-||
+
+library(ggrepel)
+plot_PCA = function (vsd.obj) {
+  pcaData <- plotPCA(vsd.obj,  intgroup = c("deseq_conditions"), returnData = T)
+  percentVar <- round(100 * attr(pcaData, "percentVar"))
+  ggplot(pcaData, aes(PC1, PC2, color=deseq_conditions)) +
+    geom_point(size=3) +
+    labs(x = paste0("PC1: ",percentVar[1],"% variance"),
+         y = paste0("PC2: ",percentVar[2],"% variance"),
+         title = "PCA Plot colored by condition") +
+    ggrepel::geom_text_repel(aes(label = name), color = "black")
+}
+plot_PCA(vsd)
+
+# //-=/-=/-=/-=/-=/-=/ PLOTTING DEG IN THE SAME TISSUE \=-\=-\=-\=-\=-\=-\=-||
+
+generate_DESeq_object <- function (my_data, groups) {
+  data_subset1 <- my_data[,grep(groups[1], colnames(my_data))]
+  data_subset2 <- my_data[,grep(groups[2], colnames(my_data))]
+  my_countData <- cbind(data_subset1, data_subset2)
+  condition <- c(rep(groups[1],ncol(data_subset1)), rep(groups[2],ncol(data_subset2)))
+  my_colData <- as.data.frame(condition)
+  rownames(my_colData) <- colnames(my_countData)
+  print(my_colData)
+  dds <- DESeqDataSetFromMatrix(countData = my_countData,
+                                colData = my_colData,
+                                design = ~ condition)
+  dds <- DESeq(dds, quiet = T)
+  return(dds)
+}
+
+tum_samples <- generate_DESeq_object(readCount, c("TUM_Control", "TUM_aPDL1"))
+adj_samples <- generate_DESeq_object(readCount, c("ADJ_Control", "ADJ_aPDL1"))
+
+tum_samples <- vst(tum_samples, blind = T)
+adj_samples <- vst(adj_samples, blind = T)
+
+a <- variable_gene_heatmap(tum_samples, 30, title = "Tumor variable genes")
+
+b <- variable_gene_heatmap(adj_samples, 30, title = "Adjacent variable genes")
+
+gridExtra::grid.arrange(a[[4]],b[[4]], nrow = 1)
+
+# //-=/-=/-=/-=/-=/-=/ GENE EXPRESSION BETWEEN SAMPLES \=-\=-\=-\=-\=-\=-\=-||
+# Find rownames that start with "IG"
+ig_genes <- rownames(dds)[grep("^IGHA", rownames(dds))]
+
+# Print the gene names
+print(ig_genes)
+
+gene <- "IGHA1"
+
+normalized_data <- counts(dds, normalized = T)
+expression <- normalized_data[gene,]
+condition <- dds@colData$deseq_conditions
+gene_tib <- tibble(condition = condition, expression = expression)
+
+ggplot(gene_tib, aes(x = condition, y = expression))+
+  geom_boxplot(outlier.size = 0)+
+  geom_point()+
+  labs (title = paste0("Expression of ", gene), x = "group", y = paste0("Normalized expression (", "Deseq2" , ")"))+
+  theme(axis.text.x = element_text(size = 16), axis.text.y = element_text(size = 12))
 
 # //-=/-=/-=/-=/-=/-=/  GSVA  \=-\=-\=-\=-\=-\=-\=-\=-||
 library(GSVA)
